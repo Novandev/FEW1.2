0548c4ee85ca7e19f3bdee62f19a3cd29f41f879
novandev <donovan.adams@students.makeschool.com>
2018-12-05 19:35:39 -0800

    added more to oregon trail

--- oregonTrail/js/Game.js
+++ oregonTrail/js/Game.js
@@ -143,3 +143,21 @@ OregonH.Game.resumeJourney = function() {
 
 //init game
 OregonH.Game.init();
+
+class Game{
+    constructor(){
+        this.WEIGHT_PER_OX = 20;
+        this.WEIGHT_PER_PERSON = 2;
+        this.FOOD_WEIGHT = 0.6;
+        this.FIREPOWER_WEIGHT = 5;
+        this.GAME_SPEED = 800;
+        this.DAY_PER_STEP = 0.2;
+        this.FOOD_PER_PERSON = 0.02;
+        this.FULL_SPEED = 5;
+        this.SLOW_SPEED = 3;
+        this.FINAL_DISTANCE = 1000;
+        this.EVENT_PROBABILITY = 0.15;
+        this.ENEMY_FIREPOWER_AVG = 5;
+        this.ENEMY_GOLD_AVG = 50;
+    }
+}
